134c8d5877c8fbbcd788f01135dd9a4946f64588
Tetsuya Mori <monry84@gmail.com>
2018-06-18 20:24:18 +0900

    :baby: Initial implementation

---
+++ Assets/Scripts/Data/Entity/TimelineEntity/ .cs
@@ -0,0 +1,11 @@
+using CAFU.Timeline.Data.Entity;
+using System;
+using TimelineName = Application.Enumerate.TimelineName. ;
+
+namespace Data.Entity.TimelineEntity
+{
+    [Serializable]
+    public class   : TimelineEntity<TimelineName>
+    {
+    }
+}
